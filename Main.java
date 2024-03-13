441fd78ac9f54192fb2180f07213110a43b8becc
Jojo-me <jojo.rw14@gmail.com>
2024-03-13 10:31:39 +0100

    Added basic Main.java to build parse tree

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Programm started");
+
+        try {
+            CharStream charStream = CharStreams.fromFileName(args[0]);
+            ExprLexer exprLexer = new ExprLexer(charStream);
+            CommonTokenStream tokenStream = new CommonTokenStream(exprLexer);
+            ExprParser exprParser = new ExprParser(tokenStream);
+
+            ParserRuleContext progContext = exprParser.prog();
+            // ParseTreeWalker.DEFAULT.walk(null, progContext);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
