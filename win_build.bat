bfbb0387fef4c08f697e917866d81f773b94b25f
SvenWlf <sven-w-3-@web.de>
2024-03-19 10:07:43 +0100

    updated win_build.bat

--- win_build.bat
+++ win_build.bat
@@ -1,4 +1,4 @@
 java -jar .\antlr-4.13.1-complete.jar -visitor -no-listener Expr.g4
 javac -cp .\antlr-4.13.1-complete.jar *.java
-java -cp "C:\Users\sven.wolf\OneDrive - Advantest\Studienjahr 2\Semester 4\Compiler\CompilerbauProject\antlr-4.13.1-complete.jar;." Main .\PipifaxCode.pfx
-java -cp "C:\Users\sven.wolf\OneDrive - Advantest\Studienjahr 2\Semester 4\Compiler\CompilerbauProject\antlr-4.13.1-complete.jar;." org.antlr.v4.gui.TestRig Expr prog -tree .\PipifaxCode.pfx
+java -cp "%CD%\antlr-4.13.1-complete.jar;." Main .\PipifaxCode.pfx
+java -cp "%CD%\antlr-4.13.1-complete.jar;." org.antlr.v4.gui.TestRig Expr prog -tree .\PipifaxCode.pfx
