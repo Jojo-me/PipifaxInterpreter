1f31eb1b4ae63beffe100f94a016ad19bbe33a88
SvenWlf <sven-w-3-@web.de>
2024-03-22 11:03:53 +0100

    Added pause to end of file

--- win_build.bat
+++ win_build.bat
@@ -2,3 +2,4 @@ java -jar .\antlr-4.13.1-complete.jar -visitor -no-listener Expr.g4
 javac -cp .\antlr-4.13.1-complete.jar *.java
 java -cp "%CD%\antlr-4.13.1-complete.jar;." Main .\PipifaxCode.pfx
 java -cp "%CD%\antlr-4.13.1-complete.jar;." org.antlr.v4.gui.TestRig Expr prog -tree .\PipifaxCode.pfx
+pause
