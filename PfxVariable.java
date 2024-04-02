565ae55072c432ab77e8eb02df45b0687ef55309
Jojo-me <jojo.rw14@gmail.com>
2024-04-02 09:20:48 +0200

    add type to variable

--- PfxVariable.java
+++ PfxVariable.java
@@ -1,3 +1,17 @@
 public class PfxVariable {
-    static public String name;
+    private final String name;
+    private final PfxType type;
+
+    PfxVariable (String name, PfxType type){
+        this.name = name;
+        this.type = type;
+    }
+
+    String getName() {
+        return name;
+    };
+
+    PfxType getType(){
+        return type;
+    }
 }
